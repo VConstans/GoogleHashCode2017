05609194420b0b3cfdf0eca058a5bb87d4e6c6c8
d.wilhelm <daniel.wilhelm@etu.unistra.fr>
2017-02-23 20:06:47 +0100

    opt2

--- daniel.py
+++ daniel.py
@@ -15,6 +15,46 @@ def fonction ():
                 break
         
 
+
+def optimisation1 ():
+    global videos,caches, requests, endpoints 
+    request.sort(key = lambda tup : tup[2]) #
+    for req in range(0,len(request)):
+        taille_vid = videos[req[0]] 
+        idendpoint = req[1]
+        endpoint = endpoints[idendpoint]
+        
+        for idcache in endpoint['caches'][0] :
+            cache = caches[idcache]
+            if (cache['freespace'] > taille_vid) :
+                caches[idcache]['videos'].append(req[0])
+                caches[idcache]['freespace'] -= taille_vid
+                break
+
+def opt_latence_endpoint():
+    global endpoints
+    for idendpoint in range(0,len(endpoints)):
+        endpoints[idendpoint]['caches'].sort( key = lambda tup : tup[1] ) 
+    
+    
+    
+def optimisation2 ():
+    global videos,caches, requests, endpoints 
+    request.sort(key = lambda tup : tup[2]) #
+    
+    opt_latence_endpoint() # optimise par les latences
+    
+    for req in range(0,len(request)):
+        taille_vid = videos[req[0]] 
+        idendpoint = req[1]
+        endpoint = endpoints[idendpoint]
+        
+        for idcache in endpoint['caches'][0] :
+            cache = caches[idcache]
+            if (cache['freespace'] > taille_vid) :
+                caches[idcache]['videos'].append(req[0])
+                caches[idcache]['freespace'] -= taille_vid
+                break
         
 # opt 1 : trier par rapport au nombre de request
 # opt 2 : trier par rapport à la taille
