3fc624ff7e45ff69fd6533e1c357583a491266d5
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2017-02-23 20:11:36 +0100

    Parsing

--- base.py
+++ base.py
@@ -2,7 +2,17 @@
 
 import sys
 
+
+videos = []
+caches = []
+requests = []
+endpoints = []
+
+
 def start():
+
+    global videos, requests, caches, endpoints
+
     # Check arguments 
     if (len(sys.argv) < 2):
         print "usage: {} <input file>".format(sys.argv[0])
@@ -10,9 +20,27 @@ def start():
 
     # Open input file
     with open(sys.argv[1], "r") as myfile:
-        # Read 10 lines 
-        for i in range(0,9):
-            myfile.readline().split()
+        nvideos, nendpoints, nreqdesc, ncaches, capcaches = [int(n) for n in myfile.readline().split()]
+        videos = [int(n) for n in myfile.readline().split()]
+
+        # Init caches
+        caches = ncaches*[{'videos':[], 'freespace': capcaches}]
+
+        # Read endpoints
+        for i in range(0,nendpoints):
+            latdc, ncachep = [int(n) for n in myfile.readline().split()]
+            endpoint = {'caches':[],'latdc': latdc}
+            # Read endpoint caches
+            for j in range(0,ncachep):
+                idcache, latcache = [int(n) for n in myfile.readline().split()]
+                endpoint['caches'].append((idcache, latcache))
+
+        # Read requests
+        for i in range(0,nreqdesc):
+            idvideo, idendpoint, nbrequests = [int(n) for n in myfile.readline().split()]
+            requests.append((idvideo, idendpoint, nbrequests))
+
+    
 
   
 start()
